ef5a541c96ab287f5f7d2a6c5ab4c1208e4b88a6
Jon Ceanfaglione <jon.ceanfaglione@octoconsulting.com>
2022-05-04 14:36:51 -0400

    added RHEL 7 stack armor bits

---
+++ scripts/rhel8/falco.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum -y install epel-release
+yum -y install make dkms
+rpm --import https://falco.org/repo/falcosecurity-3672BA8F.asc
+curl -s -o /etc/yum.repos.d/falcosecurity.repo https://falco.org/repo/falcosecurity-rpm.repo
+yum -y install kernel-devel-$(uname -r)
+yum -y install falco
