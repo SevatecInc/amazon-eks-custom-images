f01284890ed99fbce219a0c52faffb2c7db329e8
Jon Ceanfaglione <jon.ceanfaglione@octoconsulting.com>
2022-06-16 10:36:11 -0400

    rhel 8 source ami name

--- scripts/shared/firewalld.sh
+++ scripts/shared/firewalld.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 firewall-cmd --permanent --add-port=10250/tcp
+firewall-cmd --permanent --add-port=10254/tcp
 firewall-cmd --permanent --add-port=10255/tcp
 firewall-cmd --permanent --add-port=8472/udp
 firewall-cmd --permanent --add-port=30000-32767/tcp
